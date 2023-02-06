3d50aa42cee5b384ad0932071c3d3ff53dd7364b
Yokidoh <115113136+Yokidoh@users.noreply.github.com>
2023-02-05 20:51:54 -0500

    Create HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+//This is my first Java Program 251
+public class HelloWorld
+{
+   public static void main(String[]args)
+   {
+           System.out.println("Hello World!");
+         
+         String name = "George Washington";
+         
+         System.out.println(name);
+         
+         int age = 24;
+         
+         System.out.println(age);
+         
+          }
+}
